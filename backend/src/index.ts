7ba66868c07346a1e8076580d7a9111ac5dc2b36
Nadein Michael <nadein.michael@gmail.com>
2024-01-19 14:34:10 +0700

    add register

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,9 @@
-import express, { Request, Response } from "express";
-import cors from "cors";
-import "dotenv/config";
-import mongoose from "mongoose";
+import express, { Request, Response } from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import 'dotenv/config';
+
+import userRoutes from './routes/users';
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
@@ -14,10 +16,8 @@ app.use(
 );
 app.use(cors());
 
-app.get("/api/test", async (req: Request, res: Response) => {
-  res.json({ message: "Hello from express" });
-});
+app.use('/api/users', userRoutes);
 
 app.listen(3000, () => {
-  console.log("server running on localhost: 3000");
+  console.log('server running on localhost: 3000');
 });
